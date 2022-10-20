7c8a3cc5e97ad7076ef5e69050b6201671388f2d
jayukasode23 <jayukasode2398@gmail.com>
2022-10-20 21:12:45 +0530

    Added Algorithm Program

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using AlgorithmProgram;
+
+Anagram anagram = new Anagram();
+anagram.CheckAnagram();
+
+InsertionSortDemo insertion = new InsertionSortDemo();
+insertion.CheckInsertionSorting();
+
+BubbleSort bubbleSort = new BubbleSort();
+bubbleSort.CheckBobbleSorting();
+
